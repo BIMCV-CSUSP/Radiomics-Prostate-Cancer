f7e6892d495d0bebcb60700b5316216906d3d475
jesusalzate <alejoa0415@hotmail.com>
2026-04-28 08:48:12 +0200

    Changed Run.sh

--- run.sh
+++ run.sh
@@ -13,15 +13,38 @@
 module load Python/3.11.5-GCCcore-11.2.0 
 source /projects/ceib/python_enviroments/radiomics_venv/bin/activate
 
-# export PYTHONUNBUFFERED=1
+export PYTHONUNBUFFERED=1
+export MPLBACKEND=Agg
+
+python train/radiomics/2_modeling/1_train_and_evaluate.py \
+  --csv features_all_gland.csv \
+  --data_pre artifacts/radiomics \
+  --results_base results/radiomics \
+  --experiment_name more_features_v1_rerun_no_tune \
+  --feature_strategy most_discriminant \
+  --n_splits 5 \
+  --n_repeats 10 \
+  --bootstrap_iterations 1000 \
+  --ci_level 0.95 \
+  --classification_threshold 0.5 \
+  --min_features 30 \
+  --max_features_cap 100 \
+  --samples_per_feature 15 \
+  --minority_samples_per_feature 5 \
+  --fdr_alpha 0.05 \
+  --correlation_threshold 0.95 \
+  --selection_n_jobs 32 \
+  --search_n_jobs 32 \
+  --search_iterations 50 \
+  --calculate_differences \
+  --fine_tune_best_model
+
 
 # python -u train/radiomics/2_modeling/1_train_and_evaluate.py \
 #   --csv features_all_gland.csv \
 #   --data_pre artifacts/radiomics \
 #   --results_base results/radiomics \
 #   --feature_strategy most_discriminant \
-#   --n_splits 5 \
-#   --n_repeats 10 \
 #   --bootstrap_iterations 1000 \
 #   --ci_level 0.95 \
 #   --classification_threshold 0.5 \
@@ -31,31 +54,9 @@ source /projects/ceib/python_enviroments/radiomics_venv/bin/activate
 #   --minority_samples_per_feature 8 \
 #   --fdr_alpha 0.05 \
 #   --correlation_threshold 0.90 \
-#   --selection_n_jobs 32 \
-#   --search_n_jobs 32 \
+#   --selection_n_jobs 7 \
+#   --search_n_jobs 7 \
 #   --search_iterations 50 \
 #   --calculate_differences \
-#   --fine_tune_best_model
-export PYTHONUNBUFFERED=1
-export MPLBACKEND=Agg
-
-python -u train/radiomics/2_modeling/1_train_and_evaluate.py \
-  --csv features_all_gland.csv \
-  --data_pre artifacts/radiomics \
-  --results_base results/radiomics \
-  --feature_strategy most_discriminant \
-  --bootstrap_iterations 1000 \
-  --ci_level 0.95 \
-  --classification_threshold 0.5 \
-  --min_features 10 \
-  --max_features_cap 60 \
-  --samples_per_feature 25 \
-  --minority_samples_per_feature 8 \
-  --fdr_alpha 0.05 \
-  --correlation_threshold 0.90 \
-  --selection_n_jobs 7 \
-  --search_n_jobs 7 \
-  --search_iterations 50 \
-  --calculate_differences \
-  --fine_tune_best_model \
-  --postprocess_only
+#   --fine_tune_best_model \
+#   --postprocess_only
